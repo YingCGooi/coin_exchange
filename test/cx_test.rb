5086753575b29ef4c53e76e15299624b51f5e0f8
YingCGooi <ying.c.gooi@gmail.com>
2017-11-01 21:40:09 -0500

    added test_index

--- test/cx_test.rb
+++ test/cx_test.rb
@@ -16,6 +16,10 @@ class CXTest < Minitest::Test
   end
 
   def test_index
-
+    get '/'
+    assert_equal 200, last_response.status
+    assert_equal 'text/html;charset=utf-8', last_response['Content-Type']
+    assert_match /BUY AND SELL DIGITAL CURRENCIES/i, last_response.body
+    assert_match /Sign In.+\s?.+Sign Up.+\s?.+View Charts/i, last_response.body
   end
 end
