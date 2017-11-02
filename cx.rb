1090e920b2ad64666e99ff3251f504d4fa19afa6
YingCGooi <ying.c.gooi@gmail.com>
2017-11-02 17:23:15 -0500

    added chart page, building sign up form

--- cx.rb
+++ cx.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require 'chartkick'
+require 'json'
+require 'net/http'
 
 root = File.expand_path('..', __FILE__)
 
@@ -11,8 +13,34 @@ helpers do
   end
 end
 
+def parse_api(url)
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+end
+
 get '/' do
   # redirect '/dashboard' if !signed_in?
 
   erb :index
+end
+
+get '/charts' do
+  @current_bpi = 
+  parse_api('https://api.coindesk.com/v1/bpi/currentprice.json')
+  @historical_bpi = 
+    parse_api('https://api.coindesk.com/v1/bpi/historical/close.json')
+  @min_price, @max_price = @historical_bpi['bpi'].values.minmax
+
+  erb :charts
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/user/signup' do
+  username = params[:username]
+  password = params[:password]
+
 end
