a02f07e7f51aac86eace05426c84f9ba8e45797a
YingCGooi <ying.c.gooi@gmail.com>
2017-11-03 22:47:31 -0500

    added tests for sign up form, setup action to initialize admin user data

--- test/cx_test.rb
+++ test/cx_test.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test"
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'rack/test'
+require 'yaml'
 
 require_relative '../cx.rb'
 
@@ -13,6 +14,26 @@ class CXTest < Minitest::Test
 
   def app
     Sinatra::Application
+  end
+
+  def setup
+    admin_data = { 
+      "admin"=> {
+          :password=>"$2a$10$XQq2o2l8zVCndc9Ol1MpI..T9ckk2quGlRRVdXFeKJ29ySnFkkH5W",
+          :created=>"2017-11-03 22:08:11 -0500", 
+          :balances=>{:btc_bal=>0, :eth_bal=>0, :usd_bal=>6320}, 
+          :transactions=>[]
+        }
+      }
+
+    File.write(user_data_file_path, admin_data.to_yaml)
+  end
+
+  def teardown
+  end
+
+  def session
+    last_request.env['rack.session']
   end
 
   def test_index
@@ -37,5 +58,54 @@ class CXTest < Minitest::Test
     end
 
     assert_includes last_response.body, Time.now.year.to_s
+  end
+
+  def test_sign_up_page
+    get '/signup'
+    assert_equal 200, last_response.status
+    [
+      /<input type='text'.+placeholder='Username'/,
+      /<input type='password'.+placeholder='Choose A Password/,
+      /<button type='submit'>Create Account<\/button>/i
+    ]
+    .each do |pattern|
+      assert_match pattern, last_response.body
+    end
+  end
+
+  def test_sign_up_success
+    post '/user/signup', username: 'hello', password: '12345', agreed: 'true'
+    assert_equal 302, last_response.status
+    assert_equal "You have created a new account 'hello'.<br />Please sign-in to continue.", session[:success]
+    assert_includes YAML.load_file(user_data_file_path), 'hello'
+  end
+
+  def test_sign_up_error
+    post '/user/signup', username: '', password: '', agreed: nil
+    assert_equal 422, last_response.status
+    [
+      /Please enter a username/,
+      /Password must contain a non-space character/,
+      /Please accept the user agreement/
+    ]
+    .each do |pattern|
+      assert_match pattern, last_response.body
+    end
+
+    post '/user/signup', username: 'admin', password: '12345', agreed: 'true'
+    assert_equal 422, last_response.status
+    assert_match /Username 'admin' is unavailable/, last_response.body
+
+    post '/user/signup', username: 'hello world', password: '123', agreed: 'true'
+    [
+      /Username must not contain spaces/,
+      /Password too short/,
+    ]
+    .each do |pattern|
+      assert_match pattern, last_response.body
+    end
+
+    post '/user/signup', username: 'welcome', password: '12345', agreed: 'yes'
+    assert_match /Please accept the user agreement/, last_response.body
   end
 end
