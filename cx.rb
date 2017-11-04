bc07341b6f44e2be544f9d5245af4654df03a2e5
YingCGooi <ying.c.gooi@gmail.com>
2017-11-03 20:20:32 -0500

    added error validation on sign in page

--- cx.rb
+++ cx.rb
@@ -5,6 +5,7 @@ require 'chartkick'
 require 'json'
 require 'net/http'
 require 'bcrypt'
+require 'yaml'
 
 root = File.expand_path('..', __FILE__)
 
@@ -14,18 +15,35 @@ CURRENT_BPI_API = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 configure do
   enable :sessions
   set :session_secret, 'secret'
+  set :erb, :escape_html => true
 end
 
 helpers do
   def home_page?
     %w[/].include? env['REQUEST_PATH']
   end
+
+  def h(content)
+    Rack::Utils.escape_html(content)
+  end
+end
+
+before do
+  @user_data = YAML.load_file(credentials_file_path)
 end
 
 def parse_api(url)
   uri = URI(url)
   response = Net::HTTP.get(uri)
   JSON.parse(response)
+end
+
+def credentials_file_path
+  if ENV["RACK_ENV"] == "test"
+    'test/user_data.yml'
+  else
+    'user_data.yml'
+  end
 end
 
 get '/' do
@@ -49,11 +67,40 @@ get '/signup' do
 end
 
 post '/user/signup' do
-  username = params[:username]
-  password = params[:password]
-  checked  = params[:checked]
+  @username = params[:username]
+  @password = params[:password]
+  @agreed  = params[:agreed]
 
-  session[:success] = [username, password, checked]
+  errors =
+    {
+      "Please enter a username." => @username.strip.empty?,
+      "Username must not contain spaces." => @username.strip.include?(' '),
+     "Username is unavailable. Please try " => @user_data.key?(@username),
+      "Password length must be more than 3." => @password.strip.size < 3,
+      "Please accept the user agreement." => @agreed.nil?
+    }
 
-  redirect '/'
+  if errors.none? { |_, condition| condition }
+    # { 'username' => 
+    #   { 
+    #     password: 'password_hash', 
+    #     time_created: Time.now.to_s, 
+    #     balances: {btc_bal: 0, usd_bal: 10000},
+    #     transactions: ['2btc<time><buy/sell>', '5000usd<time><dep/wd>']
+    #   }
+    # }
+
+    new_user_data = {
+      @username.strip
+    }
+
+    redirect '/'
+  else
+    session[:failure] = 
+      errors.select { |_, condition| condition }
+            .keys
+            .join('<br />')
+
+    erb :signup
+  end
 end
